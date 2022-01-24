c25667a454b5f520d243f657915c9a716e914779
alberto <alapvi@yahoo.com>
2022-01-24 08:12:57 +0100

    States in invoices

--- models/invoice_model.py
+++ models/invoice_model.py
@@ -2,19 +2,40 @@
 
 from odoo import models, fields, api
 from datetime import datetime
+from odoo.exceptions import ValidationError
+
+import logging
+_logger = logging.getLogger(__name__)
 
 class InvoiceModel(models.Model):
      _name = 'invoice_app.invoice_model'
      _description = 'Invoice Model'
      _sql_constraints = [ ('invoice_unique_ref','UNIQUE (reference)','Reference must be unique!!'), ]
 
-     reference = fields.Integer(string="Ref",required=True,index=True,help="Invoice Reference")
+     reference = fields.Integer(string="Ref",required=True,default=lambda self: self._get_id(),index=True,help="Invoice Reference")
      date = fields.Date(string="Date",required=True,default=datetime.now(),help="Date")
      base = fields.Float(string="Base",compute="_check_base",help="DNI for Client", store=True)
      vat = fields.Selection(string="VAT",selection=[('0','0'),('4','4'),('10','10'),('21','21')], default='21',help="VAT for this invoice")
      total = fields.Float(string="Total",compute="_check_total",help="Total invoice",store=True)
      lines_ids = fields.One2many("invoice_app.line_model","invoice_id",string="Lines")
      client_id = fields.Many2one("invoice_app.client_model", string="Client",required=True)
+     state = fields.Selection(string="Status",selection=[('D','Draft'),('C','Confirmed')], default="D")
+
+     def confirmInvoice(self):
+          self.ensure_one()
+          if self.state == "D":
+               self.state = "C"
+               for rec in self.lines_ids:
+                    if rec.quantity <= rec.product_id.stock:
+                         rec.product_id.stock -= rec.quantity
+                    else:
+                         raise ValidationError("There is no Stock of "+rec.product_id.name+"!")          
+          return True
+
+     
+     def _get_id(self):
+          return (self.env['invoice_app.invoice_model'].search([])[-1].id + 1)
+          
 
      @api.depends("lines_ids")
      def _check_base(self):
--- models/line_model.py
+++ models/line_model.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError
 
 class LineModel(models.Model):
      _name = 'invoice_app.line_model'
@@ -10,3 +10,13 @@ class LineModel(models.Model):
      quantity = fields.Integer(string="Quantity",required=True,default=1,help="Quantity for this line")
      invoice_id = fields.Many2one("invoice_app.invoice_model",string="Invoice",help="Invoice reference")
      product_id =fields.Many2one("invoice_app.product_model",string="Product", help="Product name")
+
+     #
+     #@api.constrains("quantity")
+     #def _check_quantity(self):
+     #     for rec in self:
+     #          if rec.quantity <= rec.product_id.stock:
+     #               rec.product_id.stock -= rec.quantity
+     #               return True
+     #          raise ValidationError("There is no Stock!")
+     
